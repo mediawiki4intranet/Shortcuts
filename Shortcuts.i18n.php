90d80197303ca8d403342316fc40b59df334480c
Vitaliy Filippov <vitalif@yourcmc.ru>
2011-08-29 17:55:30 +0000

    Bug 84839 - Shortcut link output

---
+++ Shortcuts.i18n.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Vitaliy Filippov <vitalif@mail.ru>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+    'shortcut-link' => '<div style="float: right; background-color: #ccff99; margin: 0 0 0.5em 0.5em; '.
+        'padding: 5px 8px; -moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius: 10px; '.
+        'font-size: 85%; text-align: center;">Shortcut: $1</div><div style="clear: right"></div>',
+);
+
+$messages['ru'] = array(
+    'shortcut-link' => '<div style="float: right; background-color: #ccff99; margin: 0 0 0.5em 0.5em; '.
+        'padding: 5px 8px; -moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius: 10px; '.
+        'font-size: 85%; text-align: center;">Короткая ссылка: $1</div><div style="clear: right"></div>',
+);
---
+++ Shortcuts.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * MediaWiki Shortcuts extension
+ *
+ * @author Vitaliy Filippov <vitalif@mail.ru>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ *
+ * For each page, this extension tries to find a minimal length alias
+ * consisting only of latin letters and output it to the page header.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+if (!defined('MEDIAWIKI'))
+    die();
+
+$wgExtensionFunctions[] = 'wfShortcuts';
+$wgExtensionMessagesFiles['Shortcuts'] = dirname(__FILE__) . '/Shortcuts.i18n.php';
+$wgExtensionCredits['other'][] = array (
+    'name'        => 'Display shortcut links to pages',
+    'description' => 'On each page, a shortcut redirect link of minimal length will be displayed if it exists.',
+    'author'      => 'Vitaliy Filippov',
+    'url'         => 'http://wiki.4intra.net/Shortcuts_(MediaWiki)',
+    'version'     => '2011-08-29',
+);
+
+function wfShortcuts()
+{
+    global $wgHooks;
+    wfLoadExtensionMessages('Shortcuts');
+    $wgHooks['ArticleViewHeader'][] = 'efShortcutsArticleViewHeader';
+}
+
+function efShortcutsArticleViewHeader($article, &$outputDone, &$useParserCache)
+{
+    global $wgOut, $wgUser;
+    $dbr = wfGetDB(DB_SLAVE);
+    $t = $article->getTitle();
+    $res = $dbr->select(array('redirect', 'page'), '*', array(
+        'rd_namespace' => $t->getNamespace(),
+        'rd_title' => $t->getDBkey(),
+        'page_id=rd_from',
+        'page_title REGEXP \'^[\x00-\x7F]+$\'',
+    ), __METHOD__, array('ORDER BY' => 'LENGTH(rd_title) DESC', 'LIMIT' => 1));
+    $row = $res->fetchObject();
+    if ($row)
+    {
+        $title = Title::newFromRow($row);
+        if ($title->userCanRead())
+            $wgOut->addHTML(wfMsgNoTrans('shortcut-link', $wgUser->getSkin()->link($title)));
+    }
+    return true;
+}
